16d406f29ffcebd50c12f61628aca1100681a6c9
omerwk <omerwolkoon@gmail.com>
2026-07-17 21:14:16 +0300

    random update

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -3,11 +3,5 @@ function fish_greeting
     set randhello (random choice "Hello" "Hi" "Howdy" "Shalom" "G'day")
     echo $randhello (set_color magenta)Omer(set_color --reset)!    
     echo The time is (set_color green)(date +%H:%M)(set_color --reset) and it is currently (set_color blue)(curl -s wttr.in/TelAviv?format="%t") (set_color --reset)outside. 
-    set currentdate (date +%s)
-    set summerdate (date --date="20260620" +%s)
-    set sub 86400
-    set diff (math "$summerdate - $currentdate")
-    set daysleft (math --scale=0 "$diff/$sub")
-    echo There are (set_color yellow)$daysleft(set_color --reset) days left until summer break!(set_color --reset)
     echo " "
 end
